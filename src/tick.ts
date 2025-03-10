e1888950132ca8d8b7140623905c360ca3012b60
Niels Maerten <niels@maerten.be>
2025-03-10 22:04:37 +0100

    Implement tick loop

--- src/tick.ts
+++ src/tick.ts
@@ -1,15 +1,37 @@
-import config from './config';
 import logger from './logger';
+import * as nightscout from './clients/nightscout';
+import * as influxDB from './clients/influxdb';
+
+// Max number of entries in a single tick
+const MAX_ENTRIES = 100_000;
 
 export default async function onTick() {
-  logger.info('Tick');
-  logger.debug('Current configuration', config);
+  // Get date of the latest entry in InfluxDB
+  let syncedUpTo = await influxDB.getLatestEntryDate();
+  let moreData = true;
+  let entriesFetched = 0;
 
-  return new Promise<void>((resolve) => {
-    setTimeout(() => {
-      logger.info('Tick done');
-      resolve();
-    }, 60000);
-  },
-  );
+  // Loop until there is no more data to fetch
+  while (moreData) {
+    // Fetch 1 batch of data from Nightscout
+    const data = await nightscout.fetchDataSince(syncedUpTo);
+    entriesFetched += data.length;
+    syncedUpTo = data[data.length - 1].date;
+
+    // Break if less than 2 entries were fetched (prevents infinite loop)
+    if (data.length <= 1) {
+      moreData = false;
+      break;
+    }
+
+    // Break if more than MAX_ENTRIES entries were fetched
+    if (entriesFetched > MAX_ENTRIES) {
+      logger.warn(`Fetched more than ${MAX_ENTRIES} entries, deferring until next tick`);
+      moreData = false;
+      break;
+    }
+
+    // Write fetched data to InfluxDB
+    await influxDB.writeEntries(data);
+  }
 }
