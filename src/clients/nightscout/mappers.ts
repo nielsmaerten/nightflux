45155135d2adcf56cd06cf440b944eaaace0de18
Niels Maerten <niels@maerten.be>
2025-03-12 16:00:00 +0100

    feat: update mapEntry to return an array and add mapTreatment function for insulin mapping

--- src/clients/nightscout/mappers.ts
+++ src/clients/nightscout/mappers.ts
@@ -1,10 +1,10 @@
 import { NightfluxPoint } from '../influxdb/influx-types';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function mapEntry(e: any): NightfluxPoint | null {
+export function mapEntry(e: any): NightfluxPoint[] {
   const allowedTypes = ['sgv', 'mbg'];
-  if (!allowedTypes.includes(e.type)) return null;
-  return {
+  if (!allowedTypes.includes(e.type)) return [];
+  return [{
     measurement: 'glucose',
     date: new Date(e.date || e.dateString),
     tags: {
@@ -18,5 +18,28 @@ export function mapEntry(e: any): NightfluxPoint | null {
     },
     type: 'float',
     source: JSON.stringify(e),
-  };
+  }];
+}
+
+// const treatmentTypes = ['Temp Basal', 'Temporary Target', 'Bolus Wizard', 'Carb Correction', 'Profile Switch', 'Correction Bolus', 'OpenAPS Offline', ''];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function mapTreatment(e: any): NightfluxPoint[] {
+  const points: NightfluxPoint[] = [];
+  const insulin = e.insulin || 0;
+  if (insulin) points.push({
+    measurement: 'insulin',
+    date: new Date(e.created_at),
+    tags: {
+      eventType: e.eventType || 'N/A',
+      type: e.type || 'N/A',
+      isSMB: e.isSMB ? 'true' : 'false',
+    },
+    fields: {
+      insulin,
+    },
+    type: 'float',
+    source: JSON.stringify(e),
+  });
+  return points;
 }
